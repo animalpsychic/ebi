125ae6990769ea9da5f120337b8ccec631e71d66
animalpsychic <137607080+animalpsychic@users.noreply.github.com>
2025-08-13 15:09:07 +0800

    Update content.config.ts

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,26 +1,45 @@
-import { defineCollection, z } from 'astro:content';
+import { glob } from "astro/loaders";
+import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
+  // Load Markdown and MDX files in the `src/content/blog/` directory.
+  loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
+  // Type-check frontmatter using a schema
   schema: z.object({
     title: z.string(),
-    date: z.string(),
-    description: z.string().optional(),
+    description: z.string(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
   }),
 });
 
 const obat = defineCollection({
+  // Load Markdown and MDX files in the `src/content/obat/` directory.
+  loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
+  // Type-check frontmatter using a schema
   schema: z.object({
     title: z.string(),
-    date: z.string().optional(),
-    description: z.string().optional(),
+    description: z.string(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
   }),
 });
 
 const ilmu = defineCollection({
+  // Load Markdown and MDX files in the `src/content/ilmu/` directory.
+  loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
+  // Type-check frontmatter using a schema
   schema: z.object({
     title: z.string(),
-    category: z.string().optional(),
-    description: z.string().optional(),
+    description: z.string(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
   }),
 });
 
@@ -29,3 +48,4 @@ export const collections = {
   obat,
   ilmu,
 };
+
